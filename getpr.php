dd0306462b4f1df9d881cf52f9bf1393ecb0f743
isayme <isaymeorg@gmail.com>
2012-08-04 17:08:00 +0800

    add default argument, and how to use infomation

--- getpr.php
+++ getpr.php
@@ -8,7 +8,7 @@
 *   Date    :   2012-08-04
 *********************************************************/
 
-function GetPageRank($q,$host='toolbarqueries.google.com',$context=NULL) {
+function GetPageRank($q = 'www.isayme.org',$host = 'toolbarqueries.google.com',$context=NULL) {
 	$seed = "Mining PageRank is AGAINST GOOGLE'S TERMS OF SERVICE. Yes, I'm talking to you, scammer.";
 	$result = 0x01020345;
 	$len = strlen($q);
@@ -19,10 +19,19 @@ function GetPageRank($q,$host='toolbarqueries.google.com',$context=NULL) {
 	$ch=sprintf('8%08x', $result);
 	$url='http://%s/tbr?client=navclient-auto&ch=%s&features=Rank&q=info:%s';
 	$url=sprintf($url,$host,$ch,$q);
+	
+	/*$pr = '';
+    $handler = fopen($url,'r');
+    while(!feof($handler)){
+       $pr.=fread($handler,128);
+    }
+    fclose($handler);*/
+	
 	@$pr=file_get_contents($url,false,$context);
 	return $pr?substr(strrchr($pr, ':'), 1):false;
 }
 
 if (isset($_GET['q'])) { echo "Page Rank: ".GetPageRank($_GET['q']); }
+else { echo "Page Rank: ".GetPageRank(); }
 
 ?>
